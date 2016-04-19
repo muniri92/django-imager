ce9816443b3025d1ec7a20dbb37ed6e6d541d7e0
Kyle R <KyleRichVA@gmail.com>
2016-04-19 16:12:00 -0700

    whole bunch of changes to tests. revses, testing login redirects, testing user registration."

--- imagersite/imagersite/tests.py
+++ imagersite/imagersite/tests.py
@@ -1,8 +1,9 @@
 from django.test import Client
 from django.test import TestCase, override_settings
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from imager_images.models import Photo
-from .settings import BASE_DIR
+from django.conf import settings
 import factory
 import os
 
@@ -12,7 +13,7 @@ TMP_MEDIA_ROOT = 'tmp/media/'
 class PublicViewTest(TestCase):
     """Testing views not signed in."""
 
-    @override_settings(MEDIA_ROOT=os.path.join(BASE_DIR, 'tmp'))
+    @override_settings(MEDIA_ROOT=os.path.join(settings.BASE_DIR, 'tmp'))
     def setUp(self):
         self.client = Client()
         self.User = User.objects.create_user(username=u'Test_User',
@@ -24,29 +25,29 @@ class PublicViewTest(TestCase):
 
     def test_home(self):
         """Test the home view has the proper template and contains an expected image."""
-        response = self.client.get('/')
+        response = self.client.get(reverse('homepage'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
         self.assertEqual(self.photo.file, response.context['img'].file)
 
     def test_login_get(self):
-        response = self.client.get('/accounts/login', follow=True)
+        response = self.client.get(reverse('login'), follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertRedirects(response, '/accounts/login/', status_code=301)
         self.assertTemplateUsed(response, 'registration/login.html')
 
     def test_login_post(self):
-        response = self.client.post('/accounts/login',
+        response = self.client.post(reverse('login'),
                                     {'username': u'Test_User',
                                      'password': u'Secret'},
                                     follow=True)
+        self.assertTemplateUsed(response, 'success.html')
 
     def test_login_home(self):
         """Test home view has the username after login."""
-        resp = self.client.get('/')
+        resp = self.client.get(reverse('homepage'))
         self.assertNotContains(resp, self.User.username, status_code=200)
         self.client.login(username=u'Test_User', password=u'Secret')
-        resp = self.client.get('/')
+        resp = self.client.get(reverse('homepage'))
         self.assertContains(resp, self.User.username, status_code=200)
 
     def test_profile_view(self):
@@ -54,12 +55,12 @@ class PublicViewTest(TestCase):
 
         Should only display user info when logged in.
         """
-        response = self.client.get('/accounts/profile', follow=True)
+        response = self.client.get(reverse('success'))
         self.assertTemplateUsed(response, 'success.html')
         self.assertNotContains(response, self.User.username, status_code=200)
         self.assertNotContains(response, self.User.email, status_code=200)
         self.client.login(username=u'Test_User', password=u'Secret')
-        response = self.client.get('/accounts/profile', follow=True)
+        response = self.client.get(reverse('success'))
         self.assertContains(response, self.User.username, status_code=200)
         self.assertContains(response, self.User.email, status_code=200)
 
@@ -70,17 +71,31 @@ class PublicViewTest(TestCase):
         self.assertTemplateUsed(response,
                                 'registration/registration_form.html')
 
-    def test_regsitration_post(self):
+    def test_regsitration_post_good(self):
         """test post to the registration page goes through user creation."""
 
-        response = self.client.post('/accounts/register',
+        response = self.client.post('/accounts/register/',
                                     {'username': u'New_User',
-                                     'email': u'email@email.com',
-                                     'password1': u'testpw',
-                                     'password2': u'testpw'},
+                                     'email': u'user@gmail.com',
+                                     'password1': u'testpw34',
+                                     'password2': u'testpw34'},
                                     follow=True)
-        import pdb; pdb.set_trace()
+        self.assertTemplateUsed(response,
+                                'registration/registration_complete.html')
 
+    def test_regsitration_post_bad(self):
+        """test post to the registration page
+
+        Redirects back if bad form entry(bad password in this case)
+        """
+        response = self.client.post('/accounts/register/',
+                                    {'username': u'New_User',
+                                     'email': u'user@gmail.com',
+                                     'password1': u'bad',
+                                     'password2': u'bad'},
+                                    follow=True)
+        self.assertTemplateUsed(response,
+                                'registration/registration_complete.html')
 
 
 class PhotoFactory(factory.django.DjangoModelFactory):
