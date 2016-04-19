a7e17adc90511f0d942f4577a39f8711fc0a00ff
Kyle R <KyleRichVA@gmail.com>
2016-04-19 14:23:41 -0700

    refactored to django test asserts and added test for profile view.

--- imagersite/imagersite/tests.py
+++ imagersite/imagersite/tests.py
@@ -1,40 +1,68 @@
-"""Test the login and auth views."""
-from django.contrib.auth.models import User
 from django.test import Client
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from django.contrib.auth.models import User
+from imager_images.models import Photo
+from .settings import BASE_DIR
+import factory
+import os
+
+TMP_MEDIA_ROOT = 'tmp/media/'
 
 
-class TestViewProfile(TestCase):
-    """Test views class."""
+class PublicViewTest(TestCase):
+    """Testing views not signed in."""
 
+    @override_settings(MEDIA_ROOT=os.path.join(BASE_DIR, 'tmp'))
     def setUp(self):
-        """Set up a user profile for testing."""
-        # import pdb; pdb.set_trace()
-        self.user = User.objects.create_user(
-            'milkytown',
-            'milky@town.com',
-            'password'
-        )
-        self.user.save()
-        self.not_authorized = Client()
+        self.client = Client()
+        self.User = User.objects.create_user(username=u'Test_User',
+                                             email=u'test@test.com',
+                                             password=u'Secret')
+        self.photo = PhotoFactory(owner=self.User,
+                                  file=factory.django.ImageField())
+        self.photo.save()
 
-    # TEST STATUS CODES
     def test_home(self):
-        """Test the status code for a non authorized user."""
-        resp = self.not_authorized.get('/').status_code
-        self.assertEquals(resp, 200)
+        """Test the home view has the proper template and contains an expected image."""
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(self.photo.file, response.context['img'].file)
 
-    def test_registration(self):
-        """Test the status code for registration."""
-        resp = self.not_authorized.get('/accounts/register').status_code
-        self.assertEquals(resp, 301)
+    def test_login_get(self):
+        response = self.client.get('/accounts/login', follow=True)
+        self.assertEqual(response.status_code, 200)
+        # self.assertIn(('/accounts/login/', 301), response.redirect_chain)
+        self.assertRedirects(response, '/accounts/login/', status_code=301)
+        self.assertTemplateUsed(response, 'registration/login.html')
 
-    def test_login(self):
-        """Test the status code for a non authorized user."""
-        resp = self.not_authorized.get('/accounts/login').status_code
-        self.assertEquals(resp, 301)
+    def test_login_post(self):
+        response = self.client.post('/accounts/login',
+                                    {'username': u'Test_User',
+                                     'password': u'Secret'},
+                                    follow=True)
 
-    def test_profile(self):
-        """Test the status code for a non authorized user."""
-        resp = self.not_authorized.get('/accounts/success').status_code
-        self.assertEquals(resp, 404)
+    def test_login_home(self):
+        """Test home view has the username after login."""
+        resp = self.client.get('/')
+        self.assertNotContains(resp, self.User.username, status_code=200)
+        self.client.login(username=u'Test_User', password=u'Secret')
+        resp = self.client.get('/')
+        self.assertContains(resp, self.User.username, status_code=200)
+
+    def test_profile_view(self):
+        """Test profile view works as expected."""
+        response = self.client.get('/accounts/profile', follow=True)
+        self.assertTemplateUsed(response, 'success.html')
+        self.assertNotContains(response, self.User.username, status_code=200)
+        self.assertNotContains(response, self.User.email, status_code=200)
+        self.client.login(username=u'Test_User', password=u'Secret')
+        response = self.client.get('/accounts/profile', follow=True)
+        self.assertContains(response, self.User.username, status_code=200)
+        self.assertContains(response, self.User.email, status_code=200)
+
+
+
+class PhotoFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Photo
