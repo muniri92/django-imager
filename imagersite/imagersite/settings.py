52d23a827317c9363197c857e1c757fe4cce5061
Michael Sullivan <sapientia.veritas@gmail.com>
2016-04-20 17:48:17 -0700

    fixed bug with edit profile button, moved static folder to css that bitch

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'imagersite', 'static'), ]
 
 # Media file handling
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
